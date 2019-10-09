147495d14e99d298d1310cbab93529941da7e573
flori4nk <contact@flori4nk.de>
2019-10-09 19:25:56 +0200

    Add OFFCC Userscript

---
+++ official-friend-finder-comment-checker.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name      Official Friend Finder Comment Checker
+// @namespace https://github.com/r-psnfriends-mods
+// @author    Flori4nK
+// @license   MIT (https://flori4nk.de/LICENSE)    
+// @version   1
+// @grant     none
+// @match 	  https://www.reddit.com/r/*
+// ==/UserScript==
+
+document.getElementsByClassName("flat-list buttons")[0].innerHTML += "<li><button id='offcc-userscript-mainbtn'>OFFCC</button></i>";
+
+document.getElementById("offcc-userscript-mainbtn").addEventListener ("click", OFFCC_CLICK, false);
+
+var comments = document.querySelectorAll(".thing.noncollapsed.comment > .entry > form > .usertext-body > .md");
+
+function OFFCC_CLICK() {
+  var found = 0;
+  for(i = 0; i < comments.length; i++) {
+		if(!comments[i].innerText.match(/\w{1,3}\b *\/.+\/[\s\S]+/g)) {
+    	comments[i].style.background = "yellow";
+    	found++;
+    }
+  }
+  document.getElementById("offcc-userscript-mainbtn").innerText = "OFFCC: " + found;
+}
